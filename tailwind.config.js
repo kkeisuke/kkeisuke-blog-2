9dc6b54f0e6e78cce0895d707a4c6acead9c7a93
kimura keisuke <mail@kkeisuke.com>
2022-05-27 15:05:34 +0900

    npm i -D @nuxtjs/tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.warmGray,
+      blue: colors.sky
+    },
+    extend: {}
+  },
+  plugins: []
+}

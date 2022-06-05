40721df0c47dc31691e1022cfdfe8fe37cd5dca0
kimura keisuke <mail@kkeisuke.com>
2022-06-05 14:57:17 +0900

    ページ遷移におけるスクロール位置調整

--- app/router.options.ts
+++ app/router.options.ts
@@ -2,12 +2,16 @@ import type { RouterConfig } from '@nuxt/schema'
 
 // https://router.vuejs.org/api/#routeroptions
 export default <RouterConfig>{
-  // scrollBehavior (to, _, savedPosition) {
-  //   return new Promise((resolve) => {
-  //     const delay = to.hash ? 400 : 300
-  //     setTimeout(() => {
-  //       resolve(savedPosition || (to.hash ? { el: to.hash, behavior: 'smooth' } : { left: 0, top: 0 }))
-  //     }, delay)
-  //   })
-  // }
+  scrollBehavior (to, _, savedPosition) {
+    // こちらを参考にしました。
+    // https://github.com/nuxt/framework/pull/3851#issuecomment-1143960410
+    return new Promise((resolve) => {
+      const nuxtApp = useNuxtApp()
+      nuxtApp.hooks.hookOnce('page:finish', () => {
+        setTimeout(() => {
+          resolve(savedPosition || (to.hash ? { el: to.hash, behavior: 'smooth' } : { left: 0, top: 0 }))
+        }, 50)
+      })
+    })
+  }
 }
--- pages/articles/[...slug].vue
+++ pages/articles/[...slug].vue
@@ -5,9 +5,9 @@ import ArticleHeader from '@/components/articles/ArticleHeader.vue'
 import { Article } from '@/types/Article'
 
 const { title, url } = useRuntimeConfig().public
-const { path, fullPath, hash } = useRoute()
+const { path, fullPath } = useRoute()
 
-const { data: article } = await useAsyncData<Article>(`article-${path}`, () => queryContent<Article>().where({ _path: path }).findOne(), { lazy: true })
+const { data: article } = await useAsyncData<Article>(`article-${path}`, () => queryContent<Article>().where({ _path: path }).findOne())
 
 article.value && useHead({
   title: article.value.title,
@@ -19,13 +19,6 @@ article.value && useHead({
     { hid: 'ogType', property: 'og:type', content: 'article' },
     { hid: 'ogUrl', property: 'og:url', content: `${url}${fullPath}` }
   ]
-})
-
-onMounted(() => {
-  setTimeout(() => {
-    const target = hash ? document.querySelector(hash) || document.body : document.body
-    target.scrollIntoView()
-  })
 })
 </script>
 
